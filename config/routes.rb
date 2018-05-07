a289f346b4c69442b2c5112b6e2fab218a7d43aa
Barbara Rodriguez <barbs2089@gmail.com>
2018-05-07 14:26:48 +1000

    Complete form and landing pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'profiles/edit'
-  get 'profiles/show'
-  get 'pages/home'
+
+  root 'pages#home'
+
   devise_for :users
+
+  get '/profile', to: 'profiles#show'
+  post '/profile', to: 'profiles#create'
+  get '/profile/edit', to: 'profiles#edit'
+  patch '/profile', to: 'profiles#update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
