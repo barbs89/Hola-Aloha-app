5cfdb6fd9e823d5bdd38fdc3b7d683edf283ec94
Barbara Rodriguez <barbs2089@gmail.com>
2018-05-08 11:49:12 +1000

    Update request model and controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,10 +1,38 @@
 class RequestsController < ApplicationController
+  before_action :authenticate_user!, :except => [:index, :show]
+  before_action :set_request, only: [:show, :edit, :update, :delete]
+  before_action :set_profile
+ 
   def index
+    @requests = Request.all
   end
 
   def new
+    @request = Request.new
   end
 
   def create
+    @request = Request.new(request_params)
+    @request.user = current_user
+
+        if @request.save
+            flash[:notice] = 'Your request has been submitted!'
+            redirect_to requests_path
+        else
+            flash[:alert] = 'Unable to save request! Please resubmit.'
+            redirect_back fallback_location: new_request_path
+        end
   end
+
+  private
+    def request_params
+        params.require(:request).permit(:subject, :description, :lang_from, :lang_to, :price, :user_id, :file_data)
+    end
+    
+    def set_profile
+			@profile = Profile.find(current_user.id) if current_user.present?
+		end
+    def set_request
+        @request = Request.find(params[:id])
+    end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,52 @@
 class Request < ApplicationRecord
+    include ImageUploader::Attachment.new(:image)
+
+    belongs_to :user
+    has_many :profiles
+
+    validates :subject, presence: true
+    validates :description, presence: true, length: { minimum: 30 }
+    validates :user, presence: true
+    validate :language
+  
+    def language
+        if lang_from.empty?
+            self.errors[:base] << "'From' can't be left blank"
+        end
+        if lang_to.empty?
+            self.errors[:base] << "'To' can't be left blank"
+        end
+  end
+
+   LANG_OPTIONS = [ "Arabic", 
+                    "Cantonese", 
+                    "Danish", 
+                    "Dutch", 
+                    "English", 
+                    "French", 
+                    "German", 
+                    "Greek", 
+                    "Hindi", 
+                    "Indonesian", 
+                    "Italian", 
+                    "Japanese", 
+                    "Korean", 
+                    "Mandarin", 
+                    "Norwegian", 
+                    "Polish", 
+                    "Portuguese", 
+                    "Russian", 
+                    "Spanish", 
+                    "Swedish", 
+                    "Thai", 
+                    "Turkish", 
+                    "Vietnamese"]
+  
+    def self.search(search) #ここでのself.はUser.を意味する
+        if search
+            where(['lang_from LIKE ?', "%#{search}%"]) #検索とnameの部分一致を表示。User.は省略
+        else
+            all #全て表示。User.は省略
+        end
+    end
 end
